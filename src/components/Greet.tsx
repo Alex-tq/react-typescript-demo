582775d20a34a8d8e384adc27a1262e47f6b5e68
Alexander Tamarez <alextq3036@gmail.com>
2022-02-10 18:54:14 -0500

    Make a prop optional with the ? mark

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -3,15 +3,17 @@ import React from "react";
 //Defines a type of the expected props params
 type GreetProps = {
   name: string;
-  messageCount: number;
+  messageCount?: number;
   isLoggedIn: boolean;
 };
 
 function Greet(props: GreetProps) {
+  const { messageCount = 0 } = props;
+
   return (
     <div>
       {props.isLoggedIn
-        ? `Hello ${props.name} You have ${props.messageCount} messages`
+        ? `Hello ${props.name} You have ${messageCount} messages`
         : `Welcome!`}
     </div>
   );
